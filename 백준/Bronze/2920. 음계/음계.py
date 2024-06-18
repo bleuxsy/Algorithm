921963fc3ba625a69ddcd2cc19749f8ae5c2ed14
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-06-18 17:26:20 +0900

    [Bronze II] Title: 음계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,7 @@
+mylist = list(map(int, input().split()))
+if mylist == sorted(mylist) :
+  print("ascending")
+elif mylist == sorted(mylist, reverse=True):
+  print("descending")
+else:
+  print("mixed")

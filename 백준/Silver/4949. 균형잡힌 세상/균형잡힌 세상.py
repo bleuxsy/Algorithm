20acbe54482e76f48f784bb3a5730aa5f8d45537
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-07-20 17:44:42 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 340 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,26 +1,24 @@
-while True :
-    a = input()
-    stack = []
-
-    if a == "." :
+while(True):
+  word = input()
+  stack = []
+  if word ==".":
+    break
+  for data in word: 
+    if data =='[' or data =='(':
+      stack.append(data)
+    elif data ==']':
+      if len(stack)!= 0 and stack[-1] == '[':
+        stack.pop()
+      else : 
+        stack.append(']')
         break
-
-    for i in a :
-        if i == '[' or i == '(' :
-            stack.append(i)
-        elif i == ']' :
-            if len(stack) != 0 and stack[-1] == '[' :
-                stack.pop() # 맞으면 지워서 stack을 비워줌 0 = yes
-            else : 
-                stack.append(']')
-                break
-        elif i == ')' :
-            if len(stack) != 0 and stack[-1] == '(' :
-                stack.pop()
-            else :
-                stack.append(')')
-                break
-    if len(stack) == 0 :
-        print('yes')
-    else :
-        print('no')
+    elif data == ')':
+      if len(stack) != 0 and stack[-1] =='(':
+        stack.pop()
+      else:
+        stack.append(')')
+        break
+  if len(stack) == 0 :
+      print("yes")
+  else:
+      print("no")

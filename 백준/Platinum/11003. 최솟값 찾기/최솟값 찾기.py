13688ad25bbefa9bc7d3cf21b3e7770c2e9f76f0
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-07-29 17:08:27 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 6956 ms, Memory: 756540 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+N , L = map(int, input().split())
+mydeque = deque()
+A = list(map(int,input().split()))
+
+for i in range(N):
+  while mydeque and mydeque[-1][0] > A[i]:
+    mydeque.pop()
+  mydeque.append((A[i], i))
+  if mydeque[0][1] <= i-L:
+    mydeque.popleft()
+  print(mydeque[0][0], end=' ')

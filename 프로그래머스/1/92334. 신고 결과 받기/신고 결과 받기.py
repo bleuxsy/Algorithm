00ad8ca24279c8d8d6e3e6e22337f64ef84c5fe3
Seungyeon Kim <sungyeon1227@gmail.com>
2025-08-18 09:43:42 +0900

    [level 1] Title: 신고 결과 받기, Time: 4414.79 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -0,0 +1,25 @@
+def solution(id_list, report, k):
+    answer = []
+    suspends = []
+    dic= {}
+    ids = [0] * len(id_list)
+    check =[False] * len(id_list)
+    result = [0] * len(id_list)
+    for i in report:
+        a, b = i.split(" ")
+        if a not in dic:
+            dic[a] = []
+        if b not in dic[a]:
+            dic[a].append(b)
+    for key, users in dic.items():
+        for user in users:
+            ids[id_list.index(user)] += 1
+    for idx, cons in enumerate(ids):
+        if cons >= k:
+            check[idx] = True
+    for key, users in dic.items():
+        for user in users:
+            if check[id_list.index(user)]:
+                result[id_list.index(key)] += 1
+    
+    return result

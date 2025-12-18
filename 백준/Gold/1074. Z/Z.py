3c8e426656b28b01b82c126be38bff1ab4370e2a
Seungyeon Kim <sungyeon1227@gmail.com>
2025-12-18 15:30:59 +0900

    [Gold V] Title: Z, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.py
@@ -0,0 +1,23 @@
+N , r , c = map(int, input().split())
+
+res = 0
+
+while N != 0 :
+    N -= 1
+
+    if r < 2**N and c < 2**N :
+        res += 0
+
+    elif r < 2**N and c >= 2**N :
+        res += ( 2**N ) * ( 2**N) *1
+        c -= (2 **N)
+
+    elif r >= 2**N and c < 2**N :
+        res += ( 2**N ) * ( 2**N) *2
+        r -= (2 **N)
+
+    else:
+        res += (2**N)* (2**N) *3
+        c -= (2 ** N)
+        r -= (2 ** N)
+print(res)

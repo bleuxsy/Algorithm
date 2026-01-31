10f3af7f3eaf512032e9de05fe6070f8edd18747
Seungyeon Kim <sungyeon1227@gmail.com>
2026-01-31 16:07:38 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 9.37 MB -BaekjoonHub

---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,15 @@
+def solution(n, costs):
+    answer = 0
+    
+    #크루스칼 알고리즘
+    costs.sort(key = lambda x: x[2])
+    link = set([costs[0][0]])
+    while len(link) != n:
+        for c in costs:
+            if c[0] in link and c[1] in link:
+                continue
+            if c[0] in link or c[1] in link:
+                link.update([c[0], c[1]])
+                answer += c[2]
+                break
+    return answer

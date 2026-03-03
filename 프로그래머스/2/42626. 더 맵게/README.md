69b17f4a198389628ae03e2fa0de0dc6d1dc0c65
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-03 17:25:11 +0900

    [level 2] Title: 더 맵게, Time: 1954.62 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 48.9 MB, 시간: 1825.05 ms
+메모리: 50.8 MB, 시간: 1954.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 25일 09:54:49
+2026년 03월 03일 17:25:09
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,32 +1,16 @@
-# def minmax (scoville, K):
-#     return all(s >= K for s in scoville)
-import heapq 
+import heapq
+
 def solution(scoville, K):
-    answer = 0
     heapq.heapify(scoville)
-    while True:
-        min_one = heapq.heappop(scoville)
-        if min_one >= K:
-            break;
-        if len(scoville) == 0: 
+    answer = 0
+
+    while scoville and scoville[0] < K:
+        if len(scoville) < 2:
             return -1
-        min_two = heapq.heappop(scoville)
-        mixx = min_one + min_two*2
+
+        a = heapq.heappop(scoville)
+        b = heapq.heappop(scoville)
+        heapq.heappush(scoville, a + 2*b)
         answer += 1
-        heapq.heappush(scoville, mixx)
-        
-    
-    return answer
-    # while not minmax(scoville, K):
-    #     if len(scoville) < 2:
-    #         return -1
-    #     scoville = sorted(scoville, reverse=True)
-    #     print(scoville)
-    #     min_one = scoville.pop()
-    #     min_two = scoville.pop()
-    #     mixx = min_one + min_two *2
-    #     scoville.append(mixx)
-    #     print(scoville)
-    #     answer += 1
-    #     print(answer)
+
     return answer

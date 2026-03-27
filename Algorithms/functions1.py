038ebab147894940bb03b19ed8b7cf0a8c77a732
Seungyeon Kim <sungyeon1227@gmail.com>
2026-03-27 16:14:23 +0900

    Add the code

--- Algorithms/functions1.py
+++ Algorithms/functions1.py
@@ -1,52 +1,73 @@
 import heapq
+INF = 10**18
+n, m, X = map(int, input().split())
+edges = [tuple(map(int, input().split())) for _ in range(m)]
 
-n, a, b = map(int, input().split())
-grid = [list(input().strip()) for _ in range(n)]
+# Please write your code here.
+visited = [False] * (n+1)
 
-INF = 10 ** 18
-dirs = [(1, 0), (0, 1), (0, -1), (-1, 0)]
+grid = [[] for _ in range(n+1)]
+back_grid = [[] for _ in range(n+1)]
+for x,y,w in edges:
+    grid[x].append((y,w))
 
-def find(x, y):
-    D = [[INF] * n for _ in range(n)]
-    visited = [[False] * n for _ in range(n)]
-    pq = []
+for x,y, w in edges:
+    back_grid[y].append((x,w))
 
-    D[x][y] = 0
-    heapq.heappush(pq, (0, x, y))
+print(grid)
+print(back_grid)
 
-    while pq:
-        dist, sx, sy = heapq.heappop(pq)
+# [[], [(2, 4), (3, 2), (4, 7)], [(1, 1), (3, 5)], [(1, 2), (4, 4)], [(2, 3)]]
+pq = []
+D = [INF] * (n+1)
+"""
+    D : X 정점에서 다른 정점까지의 최단 거리  ( X -> ??)
+    B : 다른 정점에서 X까지의 최단 거리 (X에서 다른 정점까지의 거리) ( ?? -> X)
+    
 
-        if visited[sx][sy]:
-            continue
+"""
+heapq.heappush(pq, (0, X))
+D[X] = 0
 
-        visited[sx][sy] = True
 
-        for dx, dy in dirs:
-            nx = sx + dx
-            ny = sy + dy
 
-            if 0 <= nx < n and 0 <= ny < n:
-                if grid[nx][ny] == grid[sx][sy]:
-                    cost = a
-                else:
-                    cost = b
 
-                if D[nx][ny] > dist + cost:
-                    D[nx][ny] = dist + cost
-                    heapq.heappush(pq, (D[nx][ny], nx, ny))
+while pq :
+    w , sx = heapq.heappop(pq)
 
-    max_val = 0
-    for i in range(n):
-        for j in range(n):
-            if D[i][j] != INF:
-                max_val = max(max_val, D[i][j])
+    if visited[sx]:
+        continue
+    visited[sx] = True
 
-    return max_val
+    for nx , nw in grid[sx]:
+        if D[nx] > D[sx] + nw:
+            D[nx] = D[sx] + nw
+            heapq.heappush(pq, (D[nx], nx))
+
+pp = []
+print(D)
+visited = [False] * (n+1)
+B = [INF] * (n+1)
+heapq.heappush(pp, (0, X))
+
+B[X] = 0
+
+while pp:
+    w, sx = heapq.heappop(pp)
+
+    if visited[sx]:
+        continue
+    visited[sx] = True
+
+    for nx, nw in back_grid[sx]:
+        if B[nx] > B[sx] + nw:
+            B[nx] = B[sx] + nw
+            heapq.heappush(pp, (B[nx], nx))
 
 answer = 0
-for i in range(n):
-    for j in range(n):
-        answer = max(answer, find(i, j))
+print(B)
+for i in range(1, n+1):
+    if answer < D[i] + B[i]:
+        answer = D[i] + B[i]
 
 print(answer)

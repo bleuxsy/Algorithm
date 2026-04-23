47f4247321b35788ca1460d4a0622d54446d856f
Seungyeon Kim <sungyeon1227@gmail.com>
2026-04-23 16:04:56 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,6 +1,9 @@
 -- 코드를 입력하세요
-SELECT FOOD_TYPE , REST_ID , REST_NAME , FAVORITES
-FROM REST_INFO 
-WHERE FAVORITES IN (SELECT MAX(FAVORITES) FROM REST_INFO GROUP BY FOOD_TYPE) 
-GROUP BY FOOD_TYPE
-order by FOOD_TYPE desc;
+SELECT food_type , rest_id, rest_name , favorites
+from rest_info
+where(food_type, favorites) in (
+    select food_type, max(favorites)
+    from rest_info
+group by food_type
+)
+order by food_type desc

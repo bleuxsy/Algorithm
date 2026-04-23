b0db24ad092de8e5c2c27ad8ea0e12ac32ab26b8
Seungyeon Kim <sungyeon1227@gmail.com>
2026-04-23 16:22:17 +0900

    [level 3] Title: 없어진 기록 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,7 +1,4 @@
 -- 코드를 입력하세요
-SELECT O.ANIMAL_ID, O.NAME
-FROM ANIMAL_OUTS AS O
-left JOIN ANIMAL_INS AS I
-ON O.ANIMAL_ID = I.ANIMAL_ID
-WHERE I.ANIMAL_ID IS NULL
-ORDER BY O.ANIMAL_ID
+SELECT animal_id, name
+from animal_outs
+where animal_id not in (select animal_id from animal_ins )

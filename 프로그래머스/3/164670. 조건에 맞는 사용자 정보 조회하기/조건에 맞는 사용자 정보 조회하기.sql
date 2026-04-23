6b5183b57af0377cc6c3a140fb52fe61eb1c457e
Seungyeon Kim <sungyeon1227@gmail.com>
2026-04-23 16:20:54 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,9 +1,13 @@
 -- 코드를 입력하세요
-SELECT USER_ID , NICKNAME , CONCAT(CITY, ' ',STREET_ADDRESS1,' ', STREET_ADDRESS2) AS 전체주소, CONCAT(
-        SUBSTR(TLNO, 1, 3), '-',      
-        SUBSTR(TLNO, 4, 4), '-',     
-        SUBSTR(TLNO, 8)               
-    ) AS 전화번호
-FROM USED_GOODS_USER
-WHERE (USER_ID) IN (SELECT WRITER_ID FROM USED_GOODS_BOARD GROUP BY WRITER_ID HAVING COUNT(*) >=3 )
+SELECT user_id , nickname, 
+
+concat(city, ' ', street_address1, ' ',street_address2) as 전체주소,
+concat(substr(tlno, 1, 3) , '-', substr(tlno,4, 4) , '-', substr(tlno, 8, 4) ) as 전화번호
+from used_goods_user
+where user_id in ( 
+    select writer_id
+    from used_goods_board
+    group by writer_id
+    having count(*) >= 3
+)
 order by user_id desc

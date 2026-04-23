b9f278e9926bf10d8895694e84bde8e9aec5a15b
Seungyeon Kim <sungyeon1227@gmail.com>
2026-04-23 15:51:03 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,10 +1,19 @@
-SELECT 
-    P.PRODUCT_ID, 
-    P.PRODUCT_NAME, 
-    SUM(P.PRICE * O.AMOUNT) AS TOTAL_SALES
-FROM FOOD_PRODUCT AS P
-JOIN FOOD_ORDER AS O
-    ON P.PRODUCT_ID = O.PRODUCT_ID
-WHERE DATE_FORMAT(O.PRODUCE_DATE, '%Y-%m') = '2022-05'
-GROUP BY P.PRODUCT_ID, P.PRODUCT_NAME
-ORDER BY TOTAL_SALES DESC, P.PRODUCT_ID;
+# -- 코드를 입력하세요
+# # SELECT p.product_id, p.product_name, sum(p.price *o.amount) as total_sales
+select p.product_id, p.product_name, p.price * sum(o.amount) as total_sales
+from food_product p
+left join food_order o on p.product_id = o.product_id
+where year(o.produce_date)='2022'and month(o.produce_date) = '5'
+GROUP BY p.product_id, p.product_name, p.price
+order by total_sales desc , p.product_id
+
+# SELECT
+#     p.product_id,
+#     p.product_name,
+#     SUM(p.price * o.amount) AS total_sales
+# FROM food_product p
+# JOIN food_order o
+#     ON p.product_id = o.product_id
+# WHERE o.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+# GROUP BY p.product_id, p.product_name
+# ORDER BY total_sales DESC, p.product_id;
